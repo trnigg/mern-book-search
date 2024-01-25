d8438c783467c0440f9e8a3398f4afefb17fea78
trnigg <tim@nigg.com.au>
2024-01-26 10:05:17 +1100

    Adds GraphQL middleware to server.js

--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ const startApolloServer = async () => {
 	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json());
 
+	app.use('/graphql', expressMiddleware(server));
+
 	// if we're in production, serve client/build as static assets
 	if (process.env.NODE_ENV === 'production') {
 		app.use(express.static(path.join(__dirname, '../client/build')));
