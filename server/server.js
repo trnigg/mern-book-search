3539f559f9522bfbe4d0197c99d5dc72597fdda8
trnigg <tim@nigg.com.au>
2024-01-26 10:19:00 +1100

    Add authentications middleware

--- server/server.js
+++ server/server.js
@@ -2,17 +2,18 @@ const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes'); // TODO: Remove once graphql is implemented
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 });
-const PORT = process.env.PORT || 3001;
 
 const startApolloServer = async () => {
 	await server.start();
@@ -20,7 +21,12 @@ const startApolloServer = async () => {
 	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json());
 
-	app.use('/graphql', expressMiddleware(server));
+	app.use(
+		'/graphql',
+		expressMiddleware(server, {
+			context: authMiddleware,
+		})
+	);
 
 	// if we're in production, serve client/build as static assets
 	if (process.env.NODE_ENV === 'production') {
