9ace6503c69e9fed1aaa8905e3d70092b755891e
trnigg <tim@nigg.com.au>
2024-01-26 10:02:57 +1100

    Implements Apollo Server for GraphQL

--- server/server.js
+++ server/server.js
@@ -1,21 +1,42 @@
 const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const routes = require('./routes'); // TODO: Remove once graphql is implemented
 
 const app = express();
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startApolloServer = async () => {
+	await server.start();
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+	app.use(express.urlencoded({ extended: true }));
+	app.use(express.json());
 
-app.use(routes);
+	// if we're in production, serve client/build as static assets
+	if (process.env.NODE_ENV === 'production') {
+		app.use(express.static(path.join(__dirname, '../client/build')));
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+		// app.get('*', (req, res) => {
+		//   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+		// });
+	}
+
+	app.use(routes);
+
+	db.once('open', () => {
+		app.listen(PORT, () => {
+			console.log(`🌍 Now listening on localhost:${PORT}`);
+			console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+		});
+	});
+};
+
+startApolloServer();
