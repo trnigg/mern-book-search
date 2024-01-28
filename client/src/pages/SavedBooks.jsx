aa41671fe812a6470ec7b711edf7f653371ca859
trnigg <tim@nigg.com.au>
2024-01-29 10:48:30 +1100

    Refactors SavedBooks component to use Apollo Client

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,111 +1,104 @@
 import { useState, useEffect } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
+import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+// Import useQuery and useMutation hooks, GET_ME query, and REMOVE_BOOK mutation
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
+
+import { deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+	// const [userData, setUserData] = useState({}); NO LONGER NEEDED? useQuery hook handles?
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+	// useQuery hook to make query request
+	const { loading, data } = useQuery(GET_ME);
+	// assign data to userData on load - prevents needing to refactor jsx using userData
+	// use optional chaining operator (?) to check if data exists before setting userData
+	const userData = data?.me;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
+	// useMutation hook for removeBook mutation
+	// Don't need error variable here because we're not using it
+	const [removeBook] = useMutation(REMOVE_BOOK);
 
-        if (!token) {
-          return false;
-        }
+	// create function that accepts the book's mongo _id value as param and deletes the book from the database
+	const handleDeleteBook = async (bookId) => {
+		const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        const response = await getMe(token);
+		if (!token) {
+			return false;
+		}
 
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
+		// REFACTORED to use removeBook mutation
+		try {
+			await removeBook({
+				variables: { bookId },
+				context: {
+					headers: {
+						authorization: `Bearer ${token}`,
+					},
+				},
+			});
+			// upon success, remove book's id from localStorage
+			removeBookId(bookId);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
+	// if data isn't here yet, say so - REFACTORED to use loading variable
+	if (loading) {
+		return <h2>LOADING...</h2>;
+	}
 
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
-
-  return (
-    <>
-      <div fluid className="text-light bg-dark p-5">
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </div>
-      <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
-        <Row>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<div fluid className="text-light bg-dark p-5">
+				<Container>
+					<h1>Viewing saved books!</h1>
+				</Container>
+			</div>
+			<Container>
+				<h2 className="pt-5">
+					{userData.savedBooks.length
+						? `Viewing ${userData.savedBooks.length} saved ${
+								userData.savedBooks.length === 1 ? 'book' : 'books'
+						  }:`
+						: 'You have no saved books!'}
+				</h2>
+				<Row>
+					{userData.savedBooks.map((book) => {
+						return (
+							<Col md="4">
+								<Card key={book.bookId} border="dark">
+									{book.image ? (
+										<Card.Img
+											src={book.image}
+											alt={`The cover for ${book.title}`}
+											variant="top"
+										/>
+									) : null}
+									<Card.Body>
+										<Card.Title>{book.title}</Card.Title>
+										<p className="small">Authors: {book.authors}</p>
+										<Card.Text>{book.description}</Card.Text>
+										<Button
+											className="btn-block btn-danger"
+											onClick={() => handleDeleteBook(book.bookId)}
+										>
+											Delete this Book!
+										</Button>
+									</Card.Body>
+								</Card>
+							</Col>
+						);
+					})}
+				</Row>
+			</Container>
+		</>
+	);
 };
 
 export default SavedBooks;
