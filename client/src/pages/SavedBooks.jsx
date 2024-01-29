613a2ab25ef06a69f1c36f57519ac1438e60805b
trnigg <tim@nigg.com.au>
2024-01-29 11:07:47 +1100

    Removes unused import statement causing runtime error

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -6,7 +6,6 @@ import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
 
-import { deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
